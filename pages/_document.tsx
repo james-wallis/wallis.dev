a5c708c760c5ab4fa5f2dbc6df88d987047a7d8e
James Wallis <j@wallis.dev>
2021-02-02 23:37:49 +0000

    Add Google Analytics
    Signed-off-by: James Wallis <j@wallis.dev>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,41 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
 
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body className="bg-white dark:bg-gray-800 transition-colors duration-100 ease-linear">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+    render() {
+        const GA_MEASUREMENT_ID = 'G-612LEC91V0';
+        return (
+            <Html>
+                <Head>
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+                />
+                <script
+                    // eslint-disable-next-line react/no-danger
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${GA_MEASUREMENT_ID}', {
+                            page_path: window.location.pathname,
+                            });
+                        `,
+                    }}
+                />
+                </Head>
+                <body className="bg-white dark:bg-gray-800 transition-colors duration-100 ease-linear">
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
 
 export default MyDocument
