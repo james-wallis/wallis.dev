238bb81df8c9e3a26b2910ff5983900ff089ff52
James Wallis <j@wallis.dev>
2021-02-04 12:49:06 +0000

    Parse Dev.to markdown instead of using HTML from Dev.to, add code syntax highlighting
    Signed-off-by: James Wallis <j@wallis.dev>

---
+++ lib/markdown.ts
@@ -0,0 +1,29 @@
+import unified from 'unified';
+import parse from 'remark-parse';
+import remarkHtml from 'remark-html';
+import * as highlight from 'remark-highlight.js';
+import matter from 'gray-matter';
+
+export const sanitizeDevToMarkdown = (markdown: string) => {
+    let correctedMarkdown = '';
+
+    // Dev.to sometimes turns "# header" into "#&nbsp;header"
+    const replaceSpaceCharRegex = new RegExp(String.fromCharCode(160), "g");
+    correctedMarkdown = markdown.replace(replaceSpaceCharRegex, " ");
+
+    // Dev.to allows headers with no space after the hashtag (I don't use # on Dev.to due to the title)
+    const addSpaceAfterHeaderHashtagRegex = /##(?=[a-z|A-Z])/g;
+    return correctedMarkdown.replace(addSpaceAfterHeaderHashtagRegex, '$& ');
+}
+
+export const convertMarkdownToHtml = (markdown: string) => {
+    const { content } = matter(markdown);
+
+    const html = unified()
+        .use(parse)
+        .use(highlight)
+        .use(remarkHtml)
+        .processSync(content).contents;
+
+    return String(html);
+}
