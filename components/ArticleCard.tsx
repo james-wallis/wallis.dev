e8e46a71814864fbc52db92b52d60eee6c0e52dc
James Wallis <j@wallis.dev>
2021-02-05 21:42:50 +0000

    Make whole of article card clickable
    Signed-off-by: James Wallis <j@wallis.dev>

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -14,18 +14,18 @@ interface IProps {
 
 const ArticleCard = ({ title, description, date, tags, canonical, portfolio = false, coverImage = '' }: IProps) => (
     <div className="mb-14 flex flex-col sm:flex-row w-full">
-        <div className={`w-full text-gray-500 dark:text-gray-300 ${coverImage ? 'sm:w-1/2 lg:w-1/2 xl:w-3/5' : 'sm:w-4/5'}`}>
-            <h3 className="text-2xl text-gray-600 dark:text-gray-100">{title}</h3>
-            {!portfolio && <p className="text-sm my-1">
-                <span>{moment(date).format('Do MMMM YYYY')}</span>
-                <span className="px-1">-</span>
-                <span>{tags.join(', ')}</span>
-            </p>}
-            <p className="text-base mt-2">{description}</p>
-            <Link href={`/${portfolio ? 'portfolio' : 'blog'}/${convertCanonicalURLToRelative(canonical)}`} >
-                <a className="text-base mt-2 underline hover:text-gray-800 dark:hover:text-gray-100 transition-colors">Read more</a>
-            </Link>
-        </div>
+        <Link href={`/${portfolio ? 'portfolio' : 'blog'}/${convertCanonicalURLToRelative(canonical)}`} >
+            <a className={`w-full text-gray-500 dark:text-gray-300 ${coverImage ? 'sm:w-1/2 lg:w-1/2 xl:w-3/5' : 'sm:w-4/5'}`}>
+                <h3 className="text-2xl text-gray-600 dark:text-gray-100">{title}</h3>
+                {!portfolio && <p className="text-sm my-1">
+                    <span>{moment(date).format('Do MMMM YYYY')}</span>
+                    <span className="px-1">-</span>
+                    <span>{tags.join(', ')}</span>
+                </p>}
+                <p className="text-base mt-2">{description}</p>
+                <p className="text-base mt-2 underline hover:text-gray-800 dark:hover:text-gray-100 transition-colors">Read more</p>
+            </a>
+        </Link>
         {coverImage && <div className="w-full sm:w-1/2 lg:w-1/2 xl:w-2/5 mt-4 sm:mt-0 sm:ml-4 flex items-center justify-center">
             <img src={coverImage} className="w-full" />
         </div>}
