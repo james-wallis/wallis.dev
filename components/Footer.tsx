cc2828bd4963ce108e471d0ea60c6e7032d31de2
James Wallis <j@wallis.dev>
2021-04-16 10:40:41 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,22 @@
 import SocialIcons from './SocialIcons'
+import Link from 'next/link'
 
 const Footer = (): JSX.Element => (
     <footer className="w-full flex flex-col items-center">
         <SocialIcons />
         <a
-            className="text-base mb-8 text-gray-500 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white transition-colors"
+            className="text-sm mb-4 text-gray-500 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white transition-colors"
             href="mailto:j@wallis.dev?Subject=Hello"
         >
             j@wallis.dev
         </a>
+        <Link href="/blog/rewriting-my-website-with-a-devto-cms">
+            <a
+                className="text-sm mb-8 text-gray-500 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white transition-colors"
+            >
+                Wallis.dev is powered by the Dev.to API. Find out more.
+            </a>
+        </Link>
     </footer>
 )
 
